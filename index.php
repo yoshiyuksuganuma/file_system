543cb431c1d2fa1707c15209d3b807fe7b49fd58
yoshiyuki-suganuma <marshall3594@gmail.com>
2022-08-05 17:17:45 +0900

    Revert "add css and validation"
    This reverts commit 8c72e8a138471298d3748c809514d39c14a71340.

--- index.php
+++ index.php
@@ -3,17 +3,16 @@
 <head>
 <meta charset="utf-8">
 <title>save csv</title>
-<link rel="stylesheet" href="style.css">
 </head>
 <body>
 <h1>save csv</h1>
 <form action="/surveyDo.php" method="POST">
-    <p>件名: <input type="text" name="subject" required></p>
-    <p>お名前: <input type="text" name="name" required></p>
-    <p>ふりがな: <input type="text" name="kana" required></p>
+    <p>件名: <input type="text" name="subject"></p>
+    <p>お名前: <input type="text" name="name"></p>
+    <p>ふりがな: <input type="text" name="kana"></p>
     <p>お問い合わせ内容:</p>
-    <textarea name="contact" id="" cols="30" rows="10" required></textarea>
-    <p>メールアドレス: <input type="text" name="mail" required></p>
+    <textarea name="contact" id="" cols="30" rows="10"></textarea>
+    <p>メールアドレス: <input type="text" name="mail"></p>
     <p><button type="submit">送信</button></p>
 </form>
 <h2>ファイル内容</h2>
